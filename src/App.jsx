94e123095a6e8ac42d845e49efcca1b5d5760cc3
Sandrockcstm <sandrockcstm@yahoo.com>
2019-04-17 14:18:06 -0500

    Preparation for refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import ReactFCCtest from "react-fcctest";
 
 const operators = /[+\-x*/]/g;
 const letters = /[a-z]/i;
 const letterX = /x/gi;
-const incomplete = /$[+\-/*]/;
+const incomplete = /$[+\-/*x]/;
 
 class App extends Component {
   constructor(props) {
@@ -24,34 +25,55 @@ class App extends Component {
 
   //Logic for display
   handleInput(x) {
-    //console.log(letters.test(this.state.formula));
-    //console.log(operators.test(x));
-    //console.log(operators.test(this.state.display));
+    console.log(
+      "Result of test of display for operators is " +
+        operators.test(this.state.display) +
+        ". Display is " +
+        this.state.display
+    );
+    console.log(
+      "Result of test of x for operators is " + operators.test(x) + ". x = " + x
+    );
+
     //Checks if part of the formula is complete and the display needs to be reset
-    if (operators.test(this.state.display) && operators.test(/[0-9]\./)) {
+    if (
+      operators.test(this.state.display) &&
+      operators.test(/[0-9]\./) &&
+      operators.test(x)
+    ) {
       console.log("Calling handleFormula()");
       this.handleFormula(this.state.display);
       this.setState({ display: x });
     }
     //Doesn't allow for multiple starting 0s
     else if (this.state.display === "0" && x === "0") {
+      console.log("Multiple starting 0s not allowed");
       this.setState({ display: "0" });
       return;
     }
+    //Changes consecutive operators to only the newest
+    else if (operators.test(this.state.display) && operators.test(x)) {
+      console.log("Only one operator allowed");
+      this.setState({ display: this.state.display.replace(operators, x) });
+    }
     //Allows for operators to follow a zero
-    else if (x.match(operators)) {
+    else if (operators.test(x) && this.state.display.match()) {
+      console.log("Adding operator after 0");
       this.setState({ display: this.state.display + x });
     }
     //Removes the default 0 if preceding conditions aren't met
-    else if (x !== 0 && x !== "." && this.state.display === "0") {
+    else if (x !== "0" && x !== "." && this.state.display === "0") {
+      console.log("Removing default 0");
       this.setState({ display: x });
     }
     //Doesn't allow for consecutive decimals
     else if (this.state.display.match(/\.{1,}/g) && x === ".") {
+      console.log("Multiple decimals not allowed");
       return;
     }
     //Otherwise concat x to the string
     else {
+      console.log("handleInput is concatenating");
       this.setState({ display: this.state.display + x });
       return;
     }
@@ -74,7 +96,7 @@ class App extends Component {
       this.setState({ formula: y.replace(letterX, "*") });
       return;
     } else {
-      console.log("Concatenating");
+      console.log("handleFormula is Concatenating");
       this.setState({
         formula: this.state.formula + y.replace(letterX, "*")
       });
@@ -86,10 +108,13 @@ class App extends Component {
     if (this.state.formula === "0" || this.state.formula === "Push a button") {
       this.setState({ formula: "0", display: "0" });
       return;
-    } else if (incomplete.test(this.state.display)) {
-      console.log("You must finish the expression to evaluate it");
-      return;
     } else {
+      console.log(incomplete.test(this.state.display));
+      if (incomplete.test(this.state.display)) {
+        console.log("You must finish the expression to evaluate it");
+        this.setState({ formula: "0", display: "0" });
+        return;
+      }
       let value = eval(this.state.formula + this.state.display).toString();
       //console.log(value);
       this.setState({ display: value, formula: "0" });
@@ -101,6 +126,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <ReactFCCtest />
         <div className="col-md-6" />
         <div className="container col-md-3">
           <div className="row">
